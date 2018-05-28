b0ddfa6e8f38b1c578034db8507fbf83691d01f2
Nix <yiunixon@gmail.com>
2018-05-28 08:06:40 -0700

    renamed functions

--- main.js
+++ main.js
@@ -7,34 +7,27 @@ for (let index = 0; index < overlays.length; index++) {
   overlays[index].addEventListener("click", function(event) { showCaption(this, event); });
   overlays[index].addEventListener("mouseenter", function(event) { showCaption(this, event); });
   titles[index].addEventListener("click", function (event) { showCaption(this, event); });
-  captions[index].addEventListener("animationend", function() { changeZIndex(this) }, false);
-  captions[index].addEventListener("mouseleave", function(event) { closeCaptions(); });
+  captions[index].addEventListener("animationend", function() { hideCaption(this); }, false);
+  captions[index].addEventListener("mouseleave", function() { fadeOutCaption(); });
 }
 
-var closeCaptions = (targetCaption) => {
-  // const allCaptions = document.getElementsByClassName("caption");
-  // for (let index = 0; index < allCaptions.length; index++) {
-  //   const caption = allCaptions[index];
-  //   if (caption !== targetCaption && caption.classList[2] === "fadeIn" ) { fadeOut(caption); }
-  // }
-
+var fadeOutCaption = () => {
   const caption = document.getElementsByClassName("fadeIn")[0];
   if (caption) { fadeOut(caption); }
-  console.log(caption);
 };
 
 var showCaption = (el, event) => {
+  event.stopPropagation();
   const projectName = el.classList[1].split("-")[1];
   const caption = document.getElementsByClassName(`caption-${projectName}`)[0];
-  event.stopPropagation();
-  closeCaptions();
+  fadeOutCaption();
   fadeIn(caption);
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target.classList[0] !== "caption" && event.target.classList[0] !== "caption-text") {
-    closeCaptions();
+    fadeOutCaption();
   }
 };
 
@@ -49,7 +42,7 @@ var fadeOut = (caption) => {
   caption.classList.add("fadeOut");
 };
 
-var changeZIndex = (caption) => {
+var hideCaption = (caption) => {
   if (caption.classList[2] === "fadeOut") {
     caption.style.zIndex = "-1";
   }
